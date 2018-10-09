a832adfaf60cd2817a41d9d79c46bbc627190fc7
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2018-10-09 22:48:24 +0530

    Changes in L3 and added Deployment on pythonanywhere

--- L3/exe3/app3/views.py
+++ L3/exe3/app3/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from app3 import forms
+from django.http import HttpResponseRedirect
+#reverse(name) does the samething as {% url 'name'%}
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -13,6 +16,9 @@ def signup(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return render(request,'index.html')
+            #return render(request,'index.html')
+            #use redirect instead of render when navigating to someother site (L5)
+            #render does not change the url. ie url of index page will be url of this page
+            return HttpResponseRedirect(reverse('index'))
 
     return render(request,'signup.html',context={'form':form})
